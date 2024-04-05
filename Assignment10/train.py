00237836ce0698ff4b03cac67bed8c6d118f50d0
aiyasaswini <160306865+aiyasaswini@users.noreply.github.com>
2024-04-05 22:02:16 +0530

    Add files via upload

---
+++ Assignment10/train.py
@@ -0,0 +1,42 @@
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import torch
+import utils as utils
+
+
+def GetCorrectPredCount(pPrediction, pLabels):
+  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, criterion, train_losses, train_acc):
+  model.train()
+  pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(pbar):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+  
+
